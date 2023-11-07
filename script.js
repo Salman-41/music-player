f525a5364080673b8cac36cd7251a96bd60743c1
salman-41 <salman.yz@outlook.com>
2023-11-07 21:26:43 +0500

    play-pause

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+// Check if Playing
+let isPlaying = false;
+
+//play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Play');
+  music.play();
+}
+
+//Pause
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'pause');
+  music.pause();
+}
+
+//Play or Pause  Event Listener
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
