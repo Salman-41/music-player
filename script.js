464c272cdd1fcc549d5b8a8a1f41555fe259929d
salman-41 <salman.yz@outlook.com>
2023-11-08 21:22:40 +0500

    adding Dom element

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
