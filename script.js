bbf7c572f83f174cbfd807a31e16e6b9473852fe
salman-41 <salman.yz@outlook.com>
2023-11-08 21:50:34 +0500

    songs

--- script.js
+++ script.js
@@ -13,6 +13,21 @@ const songs = [
     displayName: 'Electric Chill Machine',
     artist: 'Jacinto Design',
   },
+  {
+    name: 'jacinto-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'jacinto-3',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+  },
 ];
 
 // Check if Playing
