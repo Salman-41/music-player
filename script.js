0184bc66a366e99e89c0dd7874b332756007daac
salman-41 <salman.yz@outlook.com>
2023-11-09 20:38:55 +0500

    progress bar

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
