8d8904a03cbbc6a66a33c4702a981809ca4ce347
salman-41 <salman.yz@outlook.com>
2023-11-11 21:10:54 +0500

    update progressBar, duration, current time

--- script.js
+++ script.js
@@ -90,7 +90,6 @@ loadSong(songs[songIndex]);
 //Update progress Bar & Time
 function updateProgressBar(e) {
   const { duration, currentTime } = e.srcElement;
-
   if (!isNaN(duration) && !isNaN(currentTime)) {
     // Update Progress Bar width
     const progressPercent = (currentTime / duration) * 100;
@@ -99,21 +98,17 @@ function updateProgressBar(e) {
     // Calculate display for duration
     const durationMinutes = Math.floor(duration / 60);
     let durationSeconds = Math.floor(duration % 60);
-
     if (durationSeconds < 10) {
       durationSeconds = `0${durationSeconds}`;
     }
-
     durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
 
     // Calculate display for current time
     const currentMinutes = Math.floor(currentTime / 60);
     let currentSeconds = Math.floor(currentTime % 60);
-
     if (currentSeconds < 10) {
       currentSeconds = `0${currentSeconds}`;
     }
-
     currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
   }
 }
